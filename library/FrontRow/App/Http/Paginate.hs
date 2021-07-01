79569c390a9c8f56642b3e37f81c0e087f2044ef
patrick brisbin <pbrisbin@gmail.com>
2021-07-01 10:36:31 -0400

    Bump to latest LTS
    This updated http-link-header with a changed interface. I had a .Compat
    module from when yesod-page-cursor went through this, so I just copied
    it in here. We could consider releasing http-link-header-compat, as is a
    common pattern for this, but I'm going to leave the copy/paste for now.

--- library/FrontRow/App/Http/Paginate.hs
+++ library/FrontRow/App/Http/Paginate.hs
@@ -55,17 +55,17 @@
 module FrontRow.App.Http.Paginate
   ( sourcePaginated
   , sourcePaginatedBy
-  )
-where
+  ) where
 
 import Prelude
 
 import Conduit
+import Control.Error.Util (hush)
 import Data.Foldable (traverse_)
 import Data.List (find)
 import Data.Maybe (listToMaybe)
 import Data.Text.Encoding (decodeUtf8)
-import Network.HTTP.Link hiding (linkHeader)
+import Network.HTTP.Link.Compat hiding (linkHeader)
 import Network.HTTP.Simple
 
 -- | Stream pages of a paginated response, using @Link@ to find next pages
@@ -97,6 +97,6 @@ sourcePaginatedBy mNextRequest runRequest req = do
 linkHeader :: Request -> Response body -> Maybe Request
 linkHeader _req resp = do
   header <- listToMaybe $ getResponseHeader "Link" resp
-  links <- parseLinkHeader $ decodeUtf8 header
+  links <- hush $ parseLinkURI $ decodeUtf8 header
   uri <- href <$> find (((Rel, "next") `elem`) . linkParams) links
   parseRequest $ show uri
