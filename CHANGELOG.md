12266fa6b0b8af76dae75b7dede8c35a0bfe605a
Evan Borden <eborden@users.noreply.github.com>
2021-11-09 11:41:35 -0600

    Use Template Haskell to produce routes names
    Yesod handles dispatch through abstract types and template haskell.
    These opaque `Route a` types makes it hard to traverse the data without
    manually unrolling the entire tree yourself. Instead we can use the same
    methods that `yesod` uses internally to traverse `ResourceTree` and
    produce the function from the route file itself.
    
    This template haskell is pretty tame, it just builds a gigantic lambda
    case expression nesting all of the routes and returning the leaf route
    name at the end.
    
    Other solutions are possible since we hold onto all `Static` and
    `Dynamic` path names in the route file, but this method matches our
    current use of `____R` resource names.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
-## [_Unreleased_](https://github.com/freckle/freckle-app/compare/v1.0.1.0...main)
+## [_Unreleased_](https://github.com/freckle/freckle-app/compare/v1.0.2.0...main)
 
 - None.
+
+## [v1.0.2.0](https://github.com/freckle/freckle-app/compare/v1.0.1.0...v1.0.2.0)
+
+- Add 'Freckle.App.Yesod.Route' to allow printing route names.
 
 ## [v1.0.1.0](https://github.com/freckle/freckle-app/compare/v1.0.0.4...v1.0.1.0)
 
---
+++ library/Freckle/App/Yesod/Routes.hs
@@ -0,0 +1,64 @@
+module Freckle.App.Yesod.Routes
+  ( mkRouteNameCaseExp
+  ) where
+
+import Prelude
+
+import Data.Foldable (fold)
+import qualified Language.Haskell.TH as TH
+import Yesod.Routes.TH.Types
+
+-- | Lambdacase expression to print route names
+--
+-- It has the following type:
+--
+-- > _ :: Route a -> String
+--
+-- It produces code like:
+--
+-- > \case
+-- >   RoutePiece a -> case a of
+-- >     RouteResource{} -> "ResourceName"
+--
+mkRouteNameCaseExp :: [ResourceTree String] -> TH.Q TH.Exp
+mkRouteNameCaseExp tree = TH.LamCaseE . fold <$> traverse mkMatches tree
+
+-- | Make match expressions for a big case over routes
+--
+-- > RoutePiece a -> case a of
+-- >   ...
+--
+mkMatches :: ResourceTree String -> TH.Q [TH.Match]
+mkMatches (ResourceLeaf resource) = pure [mkLeafMatch resource]
+mkMatches (ResourceParent name _checkOverlap params children) = do
+  caseVar <- TH.newName "a"
+  let
+    -- by convention the final param in a route is the next route constructor
+    paramVars =
+      fmap (const TH.WildP) (filter isDynamic params) <> [TH.VarP caseVar]
+  matches <- fold <$> traverse mkMatches children
+  pure
+    [ TH.Match
+        (TH.ConP constName paramVars)
+        (TH.NormalB $ TH.CaseE (TH.VarE caseVar) matches)
+        []
+    ]
+  where constName = TH.mkName name
+
+isDynamic :: Piece a -> Bool
+isDynamic = \case
+  Static{} -> False
+  Dynamic{} -> True
+
+-- | Leaf match expressions for a resource
+--
+-- > Name{} -> "ResourceName"
+--
+mkLeafMatch :: Resource String -> TH.Match
+mkLeafMatch resource = TH.Match
+  (TH.RecP constName [])
+  (TH.NormalB $ TH.LitE $ TH.StringL name)
+  []
+ where
+  constName = TH.mkName name
+  name = resourceName resource
