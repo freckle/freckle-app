d27f7fcd373aa96489ba275326edfb9d8b99e792
patrick brisbin <pbrisbin@gmail.com>
2022-06-06 10:18:22 -0400

    Refactor runCapturedLoggingT
    - Make the interface `[LogLine]`, like [`runWriterLoggingT`][rwlt]
    - Store messages in a `DList`, like `runWriterLoggingT` does
    - Update comment to indicate why we can't just use `runWriterLoggingT`
    
    [rwlt]: https://hackage.haskell.org/package/monad-logger-0.3.36/docs/Control-Monad-Logger.html#v:runWriterLoggingT

--- library/Freckle/App/Test/Logging.hs
+++ library/Freckle/App/Test/Logging.hs
@@ -1,32 +1,40 @@
 module Freckle.App.Test.Logging
-  ( runCapturedLoggingT
+  ( MonadLogger
+  , LoggingT
+  , runCapturedLoggingT
+  , logLineToText
   ) where
 
 import Freckle.App.Prelude
 
 import Control.Concurrent.Chan
 import Control.Monad.Logger
+import Data.DList (DList)
+import qualified Data.DList as DList
 import UnliftIO.Async
 import UnliftIO.Exception (finally)
 
 -- | Run a 'LoggingT', capturing and returning any logged messages alongside
 --
--- I do not know why 'runChanLoggingT' exists presumably for this purpose, but
--- requires so much more effort to ultimately accomplish.
+-- This is 'runWriterLoggingT', but we're not able to supply a 'MonadUnliftIO'
+-- instance when using that.
 --
-runCapturedLoggingT :: MonadUnliftIO m => LoggingT m a -> m (a, [Text])
+runCapturedLoggingT :: MonadUnliftIO m => LoggingT m a -> m (a, [LogLine])
 runCapturedLoggingT f = do
   chan <- liftIO newChan
-  x <- async $ captureLog [] chan
+  x <- async $ captureLog DList.empty chan
   a <- runChanLoggingT chan $ f `finally` logInfoN doneMessage
   msgs <- wait x
-  pure (a, reverse msgs)
+  pure (a, DList.toList msgs)
 
-captureLog :: MonadIO m => [Text] -> Chan (a, b, c, LogStr) -> m [Text]
+captureLog :: MonadIO m => DList LogLine -> Chan LogLine -> m (DList LogLine)
 captureLog acc chan = do
-  (_, _, _, str) <- liftIO $ readChan chan
-  let txt = decodeUtf8 $ fromLogStr str
-  if txt == doneMessage then pure acc else captureLog (txt : acc) chan
+  ll <- liftIO $ readChan chan
+  let txt = logLineToText ll
+  if txt == doneMessage then pure acc else captureLog (DList.snoc acc ll) chan
 
 doneMessage :: Text
 doneMessage = "%DONE%"
+
+logLineToText :: LogLine -> Text
+logLineToText (_, _, _, str) = decodeUtf8 $ fromLogStr str
