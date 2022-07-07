6e89a4485bd471f84e02e22afda591129c36c6f5
patrick brisbin <pbrisbin@gmail.com>
2022-07-07 10:40:24 -0400

    Compatibility back to GHC 8.4
    The extra-deps required for lts-12 make it basically lts-14 but with an
    older GHC. Still worthwhile, IMO. Using older monad-logger made possible
    by replacing our runCapturedLoggingT module with Blammo's test Logger.

--- tests/Freckle/App/MemcachedSpec.hs
+++ tests/Freckle/App/MemcachedSpec.hs
@@ -6,17 +6,17 @@ module Freckle.App.MemcachedSpec
 
 import Freckle.App.Test
 
-import Control.Lens ((^?!))
-import Control.Monad.Reader
-import Data.Aeson
-import Data.Aeson.Lens
+import Blammo.Logging.LogSettings
+import Blammo.Logging.Logger
+import Control.Lens (lens)
+import Data.Aeson (Value(..))
+import Data.Aeson.Compat as KeyMap
 import qualified Data.List.NonEmpty as NE
 import qualified Freckle.App.Env as Env
 import Freckle.App.Memcached
 import Freckle.App.Memcached.Client (MemcachedClient, newMemcachedClient)
 import qualified Freckle.App.Memcached.Client as Memcached
 import Freckle.App.Memcached.Servers
-import Freckle.App.Test.Logging
 
 data ExampleValue
   = A
@@ -36,59 +36,54 @@ instance Cachable ExampleValue where
     "C" -> Right C
     x -> Left $ "invalid: " <> show x
 
--- |
---
--- NB. we could use 'withApp' and not need to call this runner ourselves within
--- an @'it'-'example'@ -- except that we want to use 'runCapturedLoggingT' and
--- assert on the logged messages. We should add "Blammo.Logging.Test" to support
--- this use-case.
---
-runTestAppT
-  :: MonadUnliftIO m => AppExample MemcachedClient a -> m (a, [Maybe Value])
-runTestAppT f = liftIO $ do
-  mc <- loadClient
-  fmap (second $ map logLineToJSON) $ runCapturedLoggingT $ runReaderT
-    (unAppExample f)
-    mc
+data App = App
+  { appMemcachedClient :: MemcachedClient
+  , appLogger :: Logger
+  }
 
-loadClient :: IO MemcachedClient
-loadClient = do
+instance HasMemcachedClient App where
+  memcachedClientL =
+    lens appMemcachedClient $ \x y -> x { appMemcachedClient = y }
+
+instance HasLogger App where
+  loggerL = lens appLogger $ \x y -> x { appLogger = y }
+
+loadApp :: IO App
+loadApp = do
   servers <- Env.parse id $ Env.var
     (Env.eitherReader readMemcachedServers)
     "MEMCACHED_SERVERS"
     (Env.def defaultMemcachedServers)
-  newMemcachedClient servers
+
+  App <$> newMemcachedClient servers <*> newTestLogger defaultLogSettings
 
 spec :: Spec
-spec = do
+spec = withApp loadApp $ do
   describe "caching" $ do
-    it "caches the given action by key using Cachable" $ example $ do
-      void $ runTestAppT $ do
-        k <- cacheKeyThrow "A"
+    it "caches the given action by key using Cachable" $ appExample $ do
+      k <- cacheKeyThrow "A"
 
-        val <- caching k (cacheTTL 5) $ pure A
-        mbs <- Memcached.get k
+      val <- caching k (cacheTTL 5) $ pure A
+      mbs <- Memcached.get k
 
-        liftIO $ val `shouldBe` A
-        liftIO $ mbs `shouldBe` Just "A"
+      val `shouldBe` A
+      mbs `shouldBe` Just "A"
 
-    it "logs, but doesn't fail, on deserialization errors" $ example $ do
-      (_, msgs) <- runTestAppT $ do
-        k <- cacheKeyThrow "B"
+    it "logs, but doesn't fail, on deserialization errors" $ appExample $ do
+      k <- cacheKeyThrow "B"
 
-        val0 <- caching k (cacheTTL 5) $ pure B -- set
-        val1 <- caching k (cacheTTL 5) $ pure B -- get will fail
-        mbs <- Memcached.get k
+      val0 <- caching k (cacheTTL 5) $ pure B -- set
+      val1 <- caching k (cacheTTL 5) $ pure B -- get will fail
+      mbs <- Memcached.get k
 
-        liftIO $ val0 `shouldBe` B
-        liftIO $ val1 `shouldBe` B
-        liftIO $ mbs `shouldBe` Just "Broken"
+      val0 `shouldBe` B
+      val1 `shouldBe` B
+      mbs `shouldBe` Just "Broken"
 
-      let Just val = NE.last =<< NE.nonEmpty msgs
-      val ^?! key "text" . _String `shouldBe` "Error deserializing"
-      val ^?! key "meta" . key "action" . _String `shouldBe` "deserializing"
-      val
-        ^?! key "meta"
-        . key "message"
-        . _String
-        `shouldBe` "invalid: \"Broken\""
+      msgs <- getLoggedMessagesLenient
+      let Just LoggedMessage {..} = NE.last <$> NE.nonEmpty msgs
+      loggedMessageText `shouldBe` "Error deserializing"
+      loggedMessageMeta `shouldBe` KeyMap.fromList
+        [ ("action", String "deserializing")
+        , ("message", String "invalid: \"Broken\"")
+        ]
