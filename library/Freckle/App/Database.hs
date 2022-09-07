e0e35315ffc07b5d4fe44030574b93a490452171
Tom Carstens <tom.carstens@renaissance.com>
2022-09-07 11:07:21 -0500

    Export type class

--- library/Freckle/App/Database.hs
+++ library/Freckle/App/Database.hs
@@ -18,6 +18,7 @@ module Freckle.App.Database
   , postgresStatementTimeoutMilliseconds
   , envParseDatabaseConf
   , envPostgresPasswordSource
+  , HasActiveConnectionGauge
   ) where
 
 import Freckle.App.Prelude
