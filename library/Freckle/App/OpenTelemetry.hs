f3c3d413a01ce8627fbdcef351d2e822c265a5d3
patrick brisbin <pbrisbin@gmail.com>
2023-03-06 09:40:25 -0500

    Remove OpenTelemetry module, keep MonadTracer
    We're not able to move to OpenTelemetry while still using X-Ray. The
    reason is that the options for continuing to support our method of only
    sending "interesting" HTTP traces to X-Ray are severely limited. If we
    were to lose this filtering our X-Ray bill would likely increase from
    $19 dollars/month to over $400, which is unacceptable.
    
    We are planning to migrate from X-Ray to Datadog APM, at which point we
    will have more control over retention and not need this filtering.
    Meaning anything we built in the meantime would be wasted.
    
    Therefore, we're going to hold off on OpenTelemetry until we're ready to
    migrate to Datadog APM at the same time.
    
    This commit removes the actual `OpenTelemetry` module, puts our `runDB`
    back to being X-Ray-based, but changes the `HasXRayVaultData` class to
    `MonadTracer`. This means client code can be updated to use the new kind
    of constraint ahead of time (work I've already prepared), and need fewer
    changes when `OpenTelemetry` actually does enter the picture.

--- library/Freckle/App/OpenTelemetry.hs
+++ library/Freckle/App/OpenTelemetry.hs
@@ -1,117 +1,19 @@
 module Freckle.App.OpenTelemetry
-  ( HasTracer(..)
-  , Tracer
-
-  -- * Effects
-  , MonadTracer(..)
-  , inSpan
-
-  -- * Querying
-  , getCurrentTraceId
-  , getCurrentTraceIdHex
-
-  -- * Setup
-  -- ** Built-ins
-  , withTracerProviderXRay
-  , withTracerProviderXRayLogger
-  , withTracerProviderDisabled
-
-  -- ** Combinatorially
-  , withTracerProvider
-  , setupAWSXRay
-  , setupLogger
-  , disableTracing
-
-  -- ** 'Tracer'
-  , makeTracer
-  , tracerOptions
+  ( MonadTracer(..)
   ) where
 
 import Freckle.App.Prelude
 
-import Blammo.Logging
-  (LogLevel(..), Logger, Message(..), logOtherNS, runLoggerLoggingT)
-import Control.Lens ((.~), (<>~))
-import OpenTelemetry.AWSXRay
-import qualified OpenTelemetry.Context as Trace
-import qualified OpenTelemetry.Context.ThreadLocal as Trace
-import qualified OpenTelemetry.Logging.Core as Trace
-import OpenTelemetry.Trace
-  ( HasTracer(..)
-  , Tracer
-  , TracerProvider
-  , TracerProviderOptions
-  , defaultSpanArguments
-  , emptyTracerProviderOptions
-  , makeTracer
-  , tracerOptions
-  )
-import qualified OpenTelemetry.Trace.Core as Trace
-  (SpanContext(..), getSpanContext)
-import OpenTelemetry.Trace.Id (Base(Base16), TraceId, traceIdBaseEncodedText)
-import OpenTelemetry.Trace.Monad (MonadTracer(..))
-import qualified OpenTelemetry.Trace.Monad as Trace
-import OpenTelemetry.Trace.Setup
-import OpenTelemetry.Trace.Setup.Lens
+import Network.AWS.XRayClient.WAI (XRayVaultData, vaultDataFromRequest)
+import Yesod.Core (HandlerFor, waiRequest)
 
-inSpan :: (MonadUnliftIO m, MonadTracer m, HasCallStack) => Text -> m a -> m a
-inSpan = flip Trace.inSpan defaultSpanArguments
-
--- | Configure tracing with Id values compatible with AWS X-Ray
-withTracerProviderXRay :: MonadUnliftIO m => (TracerProvider -> m a) -> m a
-withTracerProviderXRay = withTracerProvider setupAWSXRay
-
--- | 'withTracerProviderXRay' but also configure to log via 'Logger'
-withTracerProviderXRayLogger
-  :: MonadUnliftIO m => Logger -> (TracerProvider -> m a) -> m a
-withTracerProviderXRayLogger logger =
-  withTracerProvider $ setupAWSXRay . setupLogger logger
-
--- | Configure no tracing
+-- | Class for reading 'XRayVaultData'
 --
--- This allows you to still make and set a 'Tracer' and satisfy 'MonadTracer',
--- but not actually emit any traces.
+-- This is named the same as the OpenTelemetry class we'll use once we move to
+-- that tracing system
 --
-withTracerProviderDisabled :: MonadUnliftIO m => (TracerProvider -> m a) -> m a
-withTracerProviderDisabled = withTracerProvider disableTracing
+class MonadTracer m where
+  getVaultData :: m (Maybe XRayVaultData)
 
-setupAWSXRay :: TracerProviderOptions -> TracerProviderOptions
-setupAWSXRay =
-  (idGeneratorL .~ awsXRayIdGenerator)
-    . (propagatorL <>~ awsXRayContextPropagator)
-
-setupLogger :: Logger -> (TracerProviderOptions -> TracerProviderOptions)
-setupLogger logger = loggerL .~ go
- where
-  go l = runLoggerLoggingT logger $ do
-    let (level, message) = fromOTelLog l
-    logOtherNS "hs-opentelemetry-sdk" level message
-
-disableTracing :: TracerProviderOptions -> TracerProviderOptions
-disableTracing = const emptyTracerProviderOptions
-
--- |
---
--- TODO: Use more fields, i.e. metadata
---
--- <https://hackage.haskell.org/package/hs-opentelemetry-api-0.0.3.6/docs/OpenTelemetry-Logging-Core.html#t:Log>
---
-fromOTelLog :: Trace.Log Text -> (LogLevel, Message)
-fromOTelLog = fromOTelSeverity . Trace.severityNumber &&& (:# []) . Trace.body
-
-fromOTelSeverity :: Maybe Int64 -> LogLevel
-fromOTelSeverity = \case
-  Just n | n > 17 -> LevelError
-  Just n | n > 13 -> LevelWarn
-  Just n | n > 9 -> LevelInfo
-  Just n | n > 1 -> LevelDebug
-  _ -> LevelOther "unknown"
-
-getCurrentTraceId :: MonadIO m => m (Maybe TraceId)
-getCurrentTraceId = do
-  mSpan <- Trace.lookupSpan <$> Trace.getContext
-  traverse (fmap Trace.traceId . Trace.getSpanContext) mSpan
-
-getCurrentTraceIdHex :: MonadIO m => m (Maybe Text)
-getCurrentTraceIdHex =
-  fmap (traceIdBaseEncodedText Base16) <$> getCurrentTraceId
+instance MonadTracer (HandlerFor app) where
+  getVaultData = vaultDataFromRequest <$> waiRequest
