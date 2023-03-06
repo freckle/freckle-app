5f809b7fc11613b706b24c7648e7c39970ddd396
patrick brisbin <pbrisbin@gmail.com>
2023-03-06 09:40:25 -0500

    Attempt to sample interesting HTTP requests

--- library/Freckle/App/OpenTelemetry.hs
+++ library/Freckle/App/OpenTelemetry.hs
@@ -20,6 +20,10 @@ module Freckle.App.OpenTelemetry
   , withTracerProvider
   , setupAWSXRay
   , setupLogger
+  , Milli
+  , milliToNano
+  , Nano
+  , setupSampling
   , disableTracing
 
   -- ** 'Tracer'
@@ -32,12 +36,20 @@ import Freckle.App.Prelude
 import Blammo.Logging
   (LogLevel(..), Logger, Message(..), logOtherNS, runLoggerLoggingT)
 import Control.Lens ((.~), (<>~))
+import Data.Coerce (coerce)
+import Data.Fixed (Milli, Nano)
+import Data.Function (on)
+import qualified Network.HTTP.Types.Status as HTTP
 import OpenTelemetry.AWSXRay
+import OpenTelemetry.Context (Context)
 import qualified OpenTelemetry.Context as Trace
 import qualified OpenTelemetry.Context.ThreadLocal as Trace
 import qualified OpenTelemetry.Logging.Core as Trace
 import OpenTelemetry.Trace
-  ( HasTracer(..)
+  ( Attribute(..)
+  , HasTracer(..)
+  , PrimitiveAttribute(..)
+  , SpanArguments(..)
   , Tracer
   , TracerProvider
   , TracerProviderOptions
@@ -46,26 +58,39 @@ import OpenTelemetry.Trace
   , makeTracer
   , tracerOptions
   )
-import qualified OpenTelemetry.Trace.Core as Trace
-  (SpanContext(..), getSpanContext)
+import qualified OpenTelemetry.Trace.Core as Trace hiding (inSpan)
 import OpenTelemetry.Trace.Id (Base(Base16), TraceId, traceIdBaseEncodedText)
 import OpenTelemetry.Trace.Monad (MonadTracer(..))
 import qualified OpenTelemetry.Trace.Monad as Trace
+import OpenTelemetry.Trace.Sampler
+  (Sampler(..), SamplingResult(..), parentBased, parentBasedOptions)
 import OpenTelemetry.Trace.Setup
 import OpenTelemetry.Trace.Setup.Lens
+import OpenTelemetry.Trace.TraceState (TraceState)
+import qualified OpenTelemetry.Trace.TraceState as TraceState
 
 inSpan :: (MonadUnliftIO m, MonadTracer m, HasCallStack) => Text -> m a -> m a
 inSpan = flip Trace.inSpan defaultSpanArguments
 
 -- | Configure tracing with Id values compatible with AWS X-Ray
 withTracerProviderXRay :: MonadUnliftIO m => (TracerProvider -> m a) -> m a
-withTracerProviderXRay = withTracerProvider setupAWSXRay
+withTracerProviderXRay =
+  withTracerProvider $ setupAWSXRay . setupSampling httpIsInteresting
 
 -- | 'withTracerProviderXRay' but also configure to log via 'Logger'
 withTracerProviderXRayLogger
   :: MonadUnliftIO m => Logger -> (TracerProvider -> m a) -> m a
 withTracerProviderXRayLogger logger =
-  withTracerProvider $ setupAWSXRay . setupLogger logger
+  withTracerProvider
+    $ setupAWSXRay
+    . setupSampling httpIsInteresting
+    . setupLogger logger
+
+httpIsInteresting :: HTTP.Status -> Nano -> Bool
+httpIsInteresting s d = HTTP.statusCode s /= 200 || d >= milliToNano 100
+
+milliToNano :: Milli -> Nano
+milliToNano = (* 1000000) . coerce
 
 -- | Configure no tracing
 --
@@ -86,6 +111,46 @@ setupLogger logger = loggerL .~ go
   go l = runLoggerLoggingT logger $ do
     let (level, message) = fromOTelLog l
     logOtherNS "hs-opentelemetry-sdk" level message
+
+-- | Set up to sample only interesting requests
+setupSampling
+  :: (HTTP.Status -> Nano -> Bool)
+  -> TracerProviderOptions
+  -> TracerProviderOptions
+setupSampling isInteresting = samplerL .~ sampler
+ where
+  sampler = parentBased $ parentBasedOptions $ Sampler
+    { getDescription = ""
+    , shouldSample = \ctxt _traceId _name args -> do
+      mTraceState <- getTraceState ctxt
+      mDuration <- getDuration args
+
+      let
+        mStatus =
+          (`HTTP.mkStatus` "")
+            . round
+            <$> getDoubleAttribute "http.status_code" args
+
+        result = case (mStatus, mDuration) of
+          (Just s, Just d) | isInteresting s d -> RecordAndSample
+          _ -> Drop
+
+      pure (result, [], fromMaybe TraceState.empty mTraceState)
+    }
+
+getTraceState :: MonadIO m => Context -> m (Maybe TraceState)
+getTraceState ctxt = fmap Trace.traceState
+  <$> traverse Trace.getSpanContext (Trace.lookupSpan ctxt)
+
+getDuration :: MonadIO m => SpanArguments -> m (Maybe Nano)
+getDuration args = for (startTime args) $ \start -> do
+  now <- Trace.getTimestamp
+  pure $ fromIntegral $ (subtract `on` Trace.timestampNanoseconds) now start
+
+getDoubleAttribute :: Text -> SpanArguments -> Maybe Double
+getDoubleAttribute name args = case lookup name (attributes args) of
+  Just (AttributeValue (DoubleAttribute x)) -> Just x
+  _ -> Nothing
 
 disableTracing :: TracerProviderOptions -> TracerProviderOptions
 disableTracing = const emptyTracerProviderOptions
