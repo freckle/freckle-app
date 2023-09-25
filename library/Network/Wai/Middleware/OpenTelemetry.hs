14cb9aa46494519926fda776211b6b501878d60a
patrick brisbin <pbrisbin@gmail.com>
2023-09-25 09:13:50 -0400

    Use upstream for DD trace-id conversion

--- library/Network/Wai/Middleware/OpenTelemetry.hs
+++ library/Network/Wai/Middleware/OpenTelemetry.hs
@@ -1,24 +1,19 @@
 module Network.Wai.Middleware.OpenTelemetry
   ( newOpenTelemetryWaiMiddleware
   , openTelemetryMiddleware
-
-    -- * Exported for testing
-  , ddTraceId
   ) where
 
 import Freckle.App.Prelude
 
 import Blammo.Logging (withThreadContext, (.=))
-import Data.Bits (Bits (shift))
 import qualified Data.ByteString.Char8 as BS8
-import qualified Data.ByteString.Short.Internal as SBI
-import Data.Primitive.ByteArray (ByteArray (ByteArray), indexByteArray)
-import Data.Word (Word64, Word8)
 import Freckle.App.OpenTelemetry
 import Network.Wai
 import Network.Wai.Middleware.AddHeaders
 import qualified OpenTelemetry.Instrumentation.Wai as Trace
-import OpenTelemetry.Trace.Id (TraceId, traceIdBytes)
+import OpenTelemetry.Propagator.Datadog
+  ( convertOpenTelemetryTraceIdToDatadogTraceId
+  )
 
 newOpenTelemetryWaiMiddleware :: IO Middleware
 newOpenTelemetryWaiMiddleware = do
@@ -38,7 +33,7 @@ openTelemetryMiddleware app request respond = do
     Nothing -> app request respond
     Just (traceId, _) -> do
       let
-        traceIdInt = ddTraceId traceId
+        traceIdInt = convertOpenTelemetryTraceIdToDatadogTraceId traceId
         traceIdIntBS = BS8.pack $ show traceIdInt
 
       withThreadContext ["trace_id" .= traceIdInt] $
@@ -47,20 +42,3 @@ openTelemetryMiddleware app request respond = do
           app
           request
           respond
-
--- | <https://github.com/iand675/hs-opentelemetry/blob/1f0328eb59fec2a97aec7ef98fe4f1e0d5c8f2ac/propagators/datadog/src/OpenTelemetry/Propagator/Datadog.hs#L103>
-ddTraceId :: TraceId -> Word64
-ddTraceId traceId = indexByteArrayNbo (ByteArray a) 1
- where
-  !(SBI.SBS a) = SBI.toShort $ traceIdBytes traceId
-
--- | <https://github.com/iand675/hs-opentelemetry/blob/1f0328eb59fec2a97aec7ef98fe4f1e0d5c8f2ac/propagators/datadog/src/OpenTelemetry/Propagator/Datadog/Internal.hs#LL109-L118C97>
-indexByteArrayNbo :: ByteArray -> Int -> Word64
-indexByteArrayNbo ba offset = loop 0 0
- where
-  loop 8 acc = acc
-  loop n acc =
-    loop (n + 1) $
-      shift acc 8
-        + fromIntegral @Word8 @Word64
-          (indexByteArray ba $ 8 * offset + n)
