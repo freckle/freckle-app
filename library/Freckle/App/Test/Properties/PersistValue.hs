96146df557f26cd6f7e440d1024cf4ad8977f453
Michael Gilliland <michael.gilliland@renaissance.com>
2024-06-27 10:44:50 -0400

    Update library/Freckle/App/Test/Properties/PersistValue.hs
    Co-authored-by: Pat Brisbin <pbrisbin@gmail.com>

--- library/Freckle/App/Test/Properties/PersistValue.hs
+++ library/Freckle/App/Test/Properties/PersistValue.hs
@@ -6,6 +6,6 @@ import Freckle.App.Prelude
 
 import Database.Persist (PersistField (..))
 
--- | Check that @fromPersistValue (toPersistValue value)@ is @value@
+-- | Check that @'fromPersistValue' ('toPersistValue' value)@ is @value@
 prop_roundTripPersistValue :: (PersistField a, Eq a) => a -> Bool
 prop_roundTripPersistValue a = fromPersistValue (toPersistValue a) == Right a
