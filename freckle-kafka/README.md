d2f35c1b94f031881ced16459d63d84ab447b6be
Chris Martin <ch.martin@gmail.com>
2025-12-16 12:34:14 -0700

    remove freckle-kafka (#261)

--- freckle-kafka/README.md
+++
@@ -1,7 +0,0 @@
-# freckle-kafka
-
-Some extensions to the [hw-kafka-client](https://hackage.haskell.org/package/hw-kafka-client) library.
-
----
-
-[CHANGELOG](./CHANGELOG.md) | [LICENSE](./LICENSE)
--- freckle-kafka/library/Freckle/App/Kafka.hs
+++
@@ -1,7 +0,0 @@
-module Freckle.App.Kafka
-  ( module Freckle.App.Kafka.Consumer
-  , module Freckle.App.Kafka.Producer
-  ) where
-
-import Freckle.App.Kafka.Consumer
-import Freckle.App.Kafka.Producer
